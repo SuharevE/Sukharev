9607d5ca2f60bbb3982de04c359af8ba76595e29
SuharevE <71093174+SuharevE@users.noreply.github.com>
2020-09-14 17:01:49 +0300

    Update script.js
    1) Скрипт умеет выбирать случайное слово из массива keywords и писать его в ПС google
    2) Скрипт начинает поиск по случайно выбранному слову
    3) Скрипт находит искомый сайт и кликает по его ссылке.

--- Bot _for_google/script.js
+++ Bot _for_google/script.js
@@ -8,25 +8,25 @@
 // @grant        none
 // ==/UserScript==
 
-let keywords=['Гобой', 'Как звучит флейта', 'Кларнет'];
+let keywords = ['Гобой','Как звучит флейта', 'Кларнет'];
 let keyword = keywords[getRandom(0,keywords.length)];
 let btnK = document.getElementsByName('btnK')[0];
 
-
-if(btnK != undefined){
+if (btnK != undefined){
     document.getElementsByName('q')[0].value = keyword;
     document.getElementsByName('btnK')[0].click();
 }else{
-let links = document.links;
+    let links = document.links;
 
-    for(let i=0; i<links.length;i++){
+    for(let i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
-        console.log("Ссылка найдена" +links[i]);
+            console.log("Ссылка найдена "+links[i]);
             links[i].click();
             break;
+        }
     }
-  }
 }
+
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
